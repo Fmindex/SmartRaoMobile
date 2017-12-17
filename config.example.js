ee26658de984f340febf7f8ac1a4ed2828e3fb7f
nai <wattanai.tha@gmail.com>
2017-12-17 17:02:32 +0700

    mobile app for smart rao

---
+++ config.example.js
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: 'your-api-key',
+  authDomain: 'smartrao-ed9fd.firebaseapp.com',
+  databaseURL: 'https://smartrao-ed9fd.firebaseio.com',
+  projectId: 'smartrao-ed9fd',
+  storageBucket: 'smartrao-ed9fd.appspot.com',
+  messagingSenderId: '1081734578218'
+};
+
+export default config;
